88df3a1e27bcd5e53816ba167009417b2768a5db
Nguyen Van Huy <Henrypoter22@gmail.com>
2020-11-23 20:15:19 +0700

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
-const btnHeader = document.getElementById("btn-header");
-const moreInfo = document.getElementById("more-info-of-class");
 
-btnHeader.addEventListener("click", (e) => {
+window.onload = (event) => {
+    const btnHeader = document.getElementById("btn-header");
+
+};
+const moreInfo = document.getElementById("more-info-of-class");
+$('#btn-header').click( (e) => {
     e.preventDefault();
     if (moreInfo.style.display == "none")
         moreInfo.style.display = "block";
-    else 
+    else
         moreInfo.style.display = "none";
 });
