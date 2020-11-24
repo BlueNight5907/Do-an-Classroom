d4993c64e4dab58d5d12562e0b770a7c4f608349
Nguyen Van Huy <Henrypoter22@gmail.com>
2020-11-24 18:36:07 +0700

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,38 @@
 
 window.onload = (event) => {
 
+    const moreInfo = document.getElementById("more-info-of-class");
+    const shareContent = document.getElementById("share-content");
+    const partition = document.getElementById("section-partition");
+    const btnCancel = document.getElementById("cancel");
+
+    $('#ost').click((event)=>{
+        $('#ost').css('background-color','#0987bd');
+        $('#ost').css('color','white');
+        setTimeout(()=>{$('#ost').css('background-color','#def6fa');
+            $('#ost').css('color','black');},200);
+    });
+    $('#btn-header').click( (e) => {
+        e.preventDefault();
+        if (moreInfo.style.display == "none")
+            moreInfo.style.display = "block";
+        else
+            moreInfo.style.display = "none";
+    });
+    $('#share-content').click( (e) => {
+        e.preventDefault();
+        shareContent.style.display = "none";
+        partition.style.display = "block";
+    });
+    $('#cancel').click( (e) => {
+        e.preventDefault();
+        shareContent.style.display = "block";
+        partition.style.display = "none";
+    });
+
+
 };
-const moreInfo = document.getElementById("more-info-of-class");
-$('#btn-header').click( (e) => {
-    e.preventDefault();
-    if (moreInfo.style.display == "none")
-        moreInfo.style.display = "block";
-    else
-        moreInfo.style.display = "none";
-});
+
 (function() {
     'use strict';
     window.addEventListener('load', function() {
@@ -34,8 +57,9 @@ function Close_attend_class(){
     $(".attend-class").hide("slow");
 }
 function Open_create_class(){
-    $(".create-class-form").show("slow");
+    $(".create-class-form-container").show("slow");
 }
 function Close_create_class(){
-    $(".create-class-form").hide("slow");
+    $(".create-class-form-container").hide("slow");
 }
+
